f3a3baee3aefbdb7aca22e3c605b225556ea2c08
Estrella Gamarra <estrellagamarra72@gmail.com>
2022-04-25 20:15:38 -0500

    Add item count

---
+++ src/components/ItemListContainer/ProductList/ItemCount.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from 'react'
+import "../ProductList/productlist.css"
+
+const ItemCount = ({ onAdd }) => {
+
+    const initial = 1;
+    const stock = 10;
+
+
+    const [count, setCount] = useState(initial);
+
+    //esta función recibe por parámetro un número que se sumará al valor inicial
+    //del estado qty. De esta manera podremos sumar y restar con una misma función
+    const addProduct = (num) => {
+        setCount(count + num);
+    };
+
+    return (
+        <div className='d-flex row justify-content-evenly w-100 m-0'>
+            <div className='d-flex justify-content-around rounded-pill bg-light w-50 py-1' style={{ backgroundColor: "#dedede" }}>
+                <button className='rounded-circle border-0 bg-white' onClick={() => addProduct(-1)}
+                    disabled={count === initial ? true : null} style={{ width: "2rem" }}>-</button>
+                <h6>{count}</h6>
+                <button className='rounded-circle border-0 bg-white'
+                    onClick={() => addProduct(+1)}
+                    disabled={count === stock ? true : null} style={{ width: "2rem" }}>+</button>
+            </div>
+
+            <button
+                className="btn addProd mt-2 rounded-bottom py-3" style={{ width: "100%" }}
+                onClick={() => onAdd(count)}
+                disabled={stock === 0 ? true : null}
+            >
+                Añadir
+            </button>
+
+        </div>
+    )
+}
+
+export default ItemCount
+
+//un state es un hook que permite almacenar valores
+//el state tiene 2 componentes
--- src/components/ItemListContainer/ProductList/ProductCard.jsx
+++ src/components/ItemListContainer/ProductList/ProductCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Img3 from '../../../assets/hp1.png'
 import "../ProductList/productlist.css"
-import ContProd from './ContProd'
+import ItemCount from './ItemCount'
 const ProductCard = () => {
+  const onAdd = (count) => {
+    alert(`Agregaste ${count} productos`);
+  };
   return (
     
   <div className="card my-3 d-flex align-items-center shadow-sm px-0" style={{width: "18rem"}}>
@@ -10,10 +13,7 @@ const ProductCard = () => {
   <div className="card-body m-0 p-0 w-100">
     <p className="card-text text-center">LAPTOP HP 15-EF1009LA</p>
     <h6 className="card-title text-center">HP</h6>
-   
-      <ContProd/>
-
-    <a href="#" className="btn addProd mt-2 rounded-bottom py-3" style={{width: "100%"}}>AGREGAR</a>
+    <ItemCount onAdd={onAdd} />
   </div>
 </div>
    
